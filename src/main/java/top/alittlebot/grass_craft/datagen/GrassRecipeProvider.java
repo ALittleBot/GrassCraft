1775b185afbba83fca6611864b235abd70bfcc69
ALittleBot <160833462+ALittleBot@users.noreply.github.com>
2024-08-04 18:56:26 +0800

    添加香草烧炼配方

--- src/main/java/top/alittlebot/grass_craft/datagen/GrassRecipeProvider.java
+++ src/main/java/top/alittlebot/grass_craft/datagen/GrassRecipeProvider.java
@@ -2,11 +2,9 @@ package top.alittlebot.grass_craft.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.jetbrains.annotations.NotNull;
 import top.alittlebot.grass_craft.GrassCraft;
@@ -31,13 +29,21 @@ public class GrassRecipeProvider {
 
         @Override
         protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
-                ShapedRecipeBuilder.shaped(RecipeCategory.MISC, GrassItems.GRASS_STICK_ITEM.get(), 4)
-                        .group(GrassCraft.MOD_ID)
-                        .define('n', Items.SHORT_GRASS)
-                        .pattern("n")
-                        .pattern("n")
-                        .unlockedBy(getHasName(Items.SHORT_GRASS), has(Items.SHORT_GRASS))
-                        .save(recipeOutput);
+            ShapedRecipeBuilder.shaped(RecipeCategory.MISC, GrassItems.GRASS_STICK_ITEM.get(), 4)
+                    .group(GrassCraft.MOD_ID)
+                    .define('#', Items.SHORT_GRASS)
+                    .pattern("#")
+                    .pattern("#")
+                    .unlockedBy(getHasName(Items.SHORT_GRASS), has(Items.SHORT_GRASS))
+                    .save(recipeOutput);
+            SimpleCookingRecipeBuilder.smelting(
+                            Ingredient.of(Items.SHORT_GRASS),
+                            RecipeCategory.MISC,
+                            GrassItems.VANILLA_ITEM.get(),
+                            0.1f,
+                            200
+                    ).unlockedBy(getHasName(Items.SHORT_GRASS), has(Items.SHORT_GRASS))
+                    .save(recipeOutput);
         }
     }
 }
