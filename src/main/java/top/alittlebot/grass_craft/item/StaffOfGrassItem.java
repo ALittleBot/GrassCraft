9988cbf073341822afcf68667f6d11a82ce7d820
ALittleBot <160833462+ALittleBot@users.noreply.github.com>
2024-08-07 21:29:47 +0800

    添加草之杖耐久

--- src/main/java/top/alittlebot/grass_craft/item/StaffOfGrassItem.java
+++ src/main/java/top/alittlebot/grass_craft/item/StaffOfGrassItem.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -19,7 +20,7 @@ import java.util.List;
 public class StaffOfGrassItem extends Item {
 
     public StaffOfGrassItem(Properties properties) {
-        super(properties);
+        super(properties.durability(32));
     }
 
     @Override
@@ -31,9 +32,9 @@ public class StaffOfGrassItem extends Item {
                 for (int z = -5; z <= 5; z++) {
                     BlockPos pos = centerPos.offset(x, 0, z);
                     BlockState blockState = serverLevel.getBlockState(pos);
-                    if (blockState.is(Blocks.GRASS_BLOCK)) {
+                    if (blockState.is(Blocks.GRASS_BLOCK) && serverLevel.getBlockState(pos.above()).canBeReplaced()) {
                         serverLevel.setBlock(pos.above(), Blocks.SHORT_GRASS.defaultBlockState(), 3);
-                    } else if (blockState.is(Blocks.DIRT)) {
+                    } else if (blockState.is(Blocks.DIRT) && serverLevel.getBlockState(pos).canBeReplaced()) {
                         serverLevel.setBlock(pos, Blocks.GRASS_BLOCK.defaultBlockState(), 3);
                     }
                 }
@@ -45,6 +46,10 @@ public class StaffOfGrassItem extends Item {
             for (LivingEntity entity : entities) {
                 entity.addEffect(new MobEffectInstance(GrassEffects.GROW_GRASS, 60 * 20, 1));
             }
+
+            ItemStack itemStack = context.getItemInHand();
+            Player player = context.getPlayer();
+            itemStack.hurtAndBreak(1, player, EquipmentSlot.MAINHAND);
 
             return InteractionResult.SUCCESS;
         }
