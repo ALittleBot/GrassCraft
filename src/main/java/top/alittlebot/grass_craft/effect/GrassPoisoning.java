dadb01518a072b7b5de6d418d77acdeebcbd733b
ALittleBot <160833462+ALittleBot@users.noreply.github.com>
2024-08-14 21:27:16 +0800

    添加草中毒二级效果啊啊啊啊啊啊啊啊啊

--- src/main/java/top/alittlebot/grass_craft/effect/GrassPoisoning.java
+++ src/main/java/top/alittlebot/grass_craft/effect/GrassPoisoning.java
@@ -2,9 +2,39 @@ package top.alittlebot.grass_craft.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 
 public class GrassPoisoning extends MobEffect {
     public GrassPoisoning(MobEffectCategory category, int color) {
         super(category, color);
+    }
+
+    @Override
+    public boolean applyEffectTick(LivingEntity entity, int amplifier) {
+        if (entity instanceof Player player) {
+            if (amplifier == 1) {
+                fillInventoryWithGrass(player);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+
+    private static void fillInventoryWithGrass(Player player) {
+        Inventory inventory = player.getInventory();
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack stack = inventory.getItem(i);
+            if (stack.isEmpty()) {
+                inventory.setItem(i, new ItemStack(Blocks.SHORT_GRASS.asItem()));
+            }
+        }
     }
 }
